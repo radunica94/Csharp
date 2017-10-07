2143bcba04adffea27d06e9a84033b6703b09ea6
radunica94 <32551335+radunica94@users.noreply.github.com>
2017-10-07 17:57:37 +0300

    Sportiv Problem

---
+++ Sportiv/UnitTestSportiv/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestSportiv")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestSportiv")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1b9f73e4-f69d-439a-8453-f5849ff0391f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Sportiv/UnitTestSportiv/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestSportiv
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void NumberOfRepetitions()
+        {
+            int totalRepetitions = CalculateRepetition(5, 4);
+            Assert.AreEqual(9,totalRepetitions);   
+        }
+
+        //firstNumberOfRepetitions reprezinta numarul de N repetitii la N runde
+        //secondNumberOfRepetitions reprezinta numarul de 1/runda de repetitii , care este N-1
+
+        int CalculateRepetition(int firstNumberOfRepetitions, int secondNumberOfRepetitions)
+        {
+            return firstNumberOfRepetitions + secondNumberOfRepetitions;
+        }
+    }
+}
