ba551d9fd24013c248befc610a4178a12ece34ad
radunica94 <32551335+radunica94@users.noreply.github.com>
2017-10-07 13:15:02 +0300

    Calculate rate for the second month

--- CSharp/BankRate/UnitTest1.cs
+++ CSharp/BankRate/UnitTest1.cs
@@ -13,11 +13,19 @@ namespace BankRate
             Assert.AreEqual(102, rate);
         }
 
+        [TestMethod]
+        public void RateForSecondMonth()
+        {
+            decimal rate = CalculateBankRate(200, 2, 12, 2);
+            Assert.AreEqual(101, rate);
+        }
+
         decimal CalculateBankRate(decimal total, int perioadInMonths, decimal interestPerYear, int currentMonth)
         {
             decimal principal = total / perioadInMonths;
             decimal exactInterestPerMonth = interestPerYear / 12 / 100;
-            return principal + total * exactInterestPerMonth;
+            decimal sold = total - (currentMonth - 1) * principal;
+            return principal + sold * exactInterestPerMonth;
         }
     }
 }
