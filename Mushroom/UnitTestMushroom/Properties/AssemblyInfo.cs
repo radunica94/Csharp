302239302e78e754a981258ec8261815b9a7d56d
radunica94 <32551335+radunica94@users.noreply.github.com>
2017-10-07 19:03:56 +0300

    Mushrooms Problem

---
+++ Mushroom/UnitTestMushroom/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestMushroom")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestMushroom")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d5db163a-f994-4c44-961e-29e33175ffcd")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Mushroom/UnitTestMushroom/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestMushroom
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void NumberOfRedMushrooms()
+        {
+            decimal total = CalculateNumberOfRedmMushrooms(100, 3);
+            Assert.AreEqual(75, total);
+        }
+
+
+        //xNumberOfMushrooms este x-ul din cerinta 
+
+        decimal CalculateNumberOfRedmMushrooms(int totalMushrooms, int xNumberOfMushrooms)
+        {
+            return (totalMushrooms * xNumberOfMushrooms) / (1 + xNumberOfMushrooms);
+        }
+    }
+}
