669c29800a3bcdd004bcbb8883aa56798cbd219d
radunica94 <32551335+radunica94@users.noreply.github.com>
2017-10-08 17:46:54 +0300

    kg of hay goats eat

---
+++ Goat/UnitTestGoat/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestGoat")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestGoat")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ee82f2dc-e1eb-4026-b287-630977455b1c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Goat/UnitTestGoat/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestGoat
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void KgOfHay()
+        {
+            decimal total = CalculateHayKg(3, 5, 105, 7, 10);
+            Assert.AreEqual(490, total);
+        }
+        
+
+        decimal CalculateHayKg(int xDays , int yGoats, int zKg , int wDays , int qGoats)
+        {
+
+            //number of hay eaten by y goats in w days
+            decimal hayInWDays = wDays * zKg / xDays;
+
+            //return the number of hay eaten by q goats in w days;
+            return hayInWDays*qGoats/yGoats;
+        }
+
+    }
+}
