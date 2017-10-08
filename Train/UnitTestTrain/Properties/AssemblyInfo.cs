5a09ef2f8e194e6f0e3b60ba548e27d67b4d3fa0
radunica94 <32551335+radunica94@users.noreply.github.com>
2017-10-08 18:52:44 +0300

    bird distance calculation

---
+++ Train/UnitTestTrain/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestTrain")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestTrain")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("db25b9d8-5244-4476-8763-3b162b18b1ae")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Train/UnitTestTrain/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestTrain
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void DistanceBird()
+        {
+            float distance = CalculateDistance(20, 80);
+            Assert.AreEqual(40, distance);
+        }
+        
+        float CalculateDistance(float trainSpeed, float trainDistance)
+        {
+            float birdSpeed = 2 * trainSpeed;
+            float birdTime = trainDistance / birdSpeed - ((trainDistance / 4)/trainSpeed);
+            float birdDistance = birdSpeed * birdTime; 
+            return birdDistance;
+        }
+    }
+}
