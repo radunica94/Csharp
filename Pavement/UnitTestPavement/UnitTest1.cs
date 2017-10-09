db7b3cffc212f3c642d7faa39e1792d013a94e7d
radunica94 <32551335+radunica94@users.noreply.github.com>
2017-10-09 21:19:32 +0300

    Number of stones tests

---
+++ Pavement/UnitTestPavement/UnitTest1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestPavement
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            double total = CalculateNumberOfStones(6, 6, 2);
+            Assert.AreEqual(9, total);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            double total = CalculateNumberOfStones(6, 6, 4);
+            Assert.AreEqual(4, total);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            double total = CalculateNumberOfStones(6, 6, 3);
+            Assert.AreEqual(4, total);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            double total = CalculateNumberOfStones(10, 10, 2);
+            Assert.AreEqual(25, total);
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            double total = CalculateNumberOfStones(6, 5, 3);
+            Assert.AreEqual(4, total);
+        }
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+            double total = CalculateNumberOfStones(4, 4, 4);
+            Assert.AreEqual(1, total);
+        }
+
+        double CalculateNumberOfStones(double streetLenght, double streetWidth , double stoneDimension)
+        {
+            double result = 0;
+            if(streetLenght % stoneDimension == 0 && streetWidth % stoneDimension == 0)
+            {
+                double lenght = streetLenght / stoneDimension;
+                double widht = streetWidth / stoneDimension;
+                result = lenght * widht;
+            }
+            else
+            {
+                double lenght = Math.Ceiling(streetLenght / stoneDimension);
+                double widht = Math.Ceiling(streetWidth / stoneDimension);
+                result = lenght*widht;
+            }
+            return result;        
+        }
+    }
+}
