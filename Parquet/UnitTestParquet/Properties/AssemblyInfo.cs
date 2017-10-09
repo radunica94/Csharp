9bfb68ad198760fa2b1756f09a7ed1e55b0d4f9d
radunica94 <32551335+radunica94@users.noreply.github.com>
2017-10-09 18:15:00 +0300

    first test

---
+++ Parquet/UnitTestParquet/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestParquet")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestParquet")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cc3f274d-deac-4ad9-989c-2a59db9099c7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Parquet/UnitTestParquet/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestParquet
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void PlateNumber()
+        {
+            double total = CalculateNumberOfParcquetPlates(17,4,2,1);
+            Assert.AreEqual(40, total);
+        }
+
+
+
+        double CalculateNumberOfParcquetPlates(double roomLenght , double roomWidth , double plateLenght, double plateWidth)
+        {
+            double roomArea = roomLenght * roomWidth;
+            double plateArea = plateLenght * plateWidth;
+            return roomArea / (plateArea - plateArea * 0.15);
+        }
+    }
+}
