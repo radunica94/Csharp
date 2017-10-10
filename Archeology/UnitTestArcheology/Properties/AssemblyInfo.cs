3e1d539783451ff4588071ea340a040fa265741f
radunica94 <32551335+radunica94@users.noreply.github.com>
2017-10-11 01:09:41 +0300

    archeology first and second test

---
+++ Archeology/UnitTestArcheology/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestArcheology")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestArcheology")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("62ea9dd9-41b1-45ee-b341-0ecd343cf4b5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Archeology/UnitTestArcheology/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestArcheology
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            double area = CalculateBuildingArea(0, 0, 2, 0, 1, 2);
+            Assert.AreEqual(2, area);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            double area = CalculateBuildingArea(2, 0, 0, 0, 1, 2);
+            Assert.AreEqual(2, area);
+        }
+
+        double CalculateBuildingArea(double x1, double y1, double x2, double y2, double x3, double y3)
+        {
+            double xy = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            double xz = Math.Sqrt(Math.Pow(x3 - x1, 2) + Math.Pow(y3 - y1, 2));
+            double yz = Math.Sqrt(Math.Pow(x3 - x2, 2) + Math.Pow(y3 - y2, 2));
+
+            double p = (xy + xz + yz) / 2;
+
+            double S = Math.Sqrt((p * (p - xy) * (p - xz) * (p - yz)));
+
+            return S;
+        }
+    }
+}
