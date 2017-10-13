66aead9db388e9a374d0a0f5f98964ca3fc5b6fe
radunica94 <32551335+radunica94@users.noreply.github.com>
2017-10-13 18:02:43 +0300

    Calculate daytime taxi fare for short distance

---
+++ TaxiFare/UnitTestTaxiFare/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestTaxiFare")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestTaxiFare")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("84bb799a-3d61-4759-bc9d-42ea7cf8ac0f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TaxiFare/UnitTestTaxiFare/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestTaxiFare
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void DayTimeFareForShortDistances()
+        {
+                Assert.AreEqual(5, CalculateTaxiFare(1, 8));
+        }
+
+        decimal CalculateTaxiFare(int distanceInKm, int hour)
+        {
+            return distanceInKm * 5;
+        }
+    }
+}
