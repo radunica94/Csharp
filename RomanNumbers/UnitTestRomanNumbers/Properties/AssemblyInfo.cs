ba6f5c8f13d0996f3ace9853f80038ecc771adb3
radunica94 <32551335+radunica94@users.noreply.github.com>
2017-10-13 19:25:24 +0300

    ConvertToROmanNumber function

---
+++ RomanNumbers/UnitTestRomanNumbers/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestRomanNumbers")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestRomanNumbers")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("80f511ea-14fd-429a-bdd9-b98392f83beb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ RomanNumbers/UnitTestRomanNumbers/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestRomanNumbers
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual("X", ConvertToRomanNumber(10));
+        }
+
+        string ConvertToRomanNumber(int numberToConvert)
+        {
+            string convertedNumber = null;
+
+
+            return convertedNumber;
+        }
+    }
+}
