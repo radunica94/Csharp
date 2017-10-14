c9e271c53d053d834407b5df8c6c85aac18783bc
radunica94 <32551335+radunica94@users.noreply.github.com>
2017-10-14 17:34:26 +0300

    testing more numbers to see if the function works good

--- RomanNumbers/UnitTestRomanNumbers/UnitTest1.cs
+++ RomanNumbers/UnitTestRomanNumbers/UnitTest1.cs
@@ -24,12 +24,29 @@ namespace UnitTestRomanNumbers
             Assert.AreEqual("C", ConvertToRomanNumber(100));
         }
 
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Assert.AreEqual("XCV", ConvertToRomanNumber(95));
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            Assert.AreEqual("LXI", ConvertToRomanNumber(61));
+        }
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+            Assert.AreEqual("LXXV", ConvertToRomanNumber(75));
+        }
+
         string ConvertToRomanNumber(int numberToConvert)
         {
             string[] hundredsLetters = { "", "C" };
             string[] tensLetters = { "", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC" };
             string[] onesLetters = { "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" };
-
             string romanValue = "";
             int number;
             //numbers with 3 decimals
