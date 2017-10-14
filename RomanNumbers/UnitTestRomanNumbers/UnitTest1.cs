2761c5584056d59b42ceca20ba8e90aa13a0906e
radunica94 <32551335+radunica94@users.noreply.github.com>
2017-10-14 17:32:15 +0300

    third test

--- RomanNumbers/UnitTestRomanNumbers/UnitTest1.cs
+++ RomanNumbers/UnitTestRomanNumbers/UnitTest1.cs
@@ -12,12 +12,40 @@ namespace UnitTestRomanNumbers
             Assert.AreEqual("X", ConvertToRomanNumber(10));
         }
 
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.AreEqual("XXII", ConvertToRomanNumber(22));
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Assert.AreEqual("C", ConvertToRomanNumber(100));
+        }
+
         string ConvertToRomanNumber(int numberToConvert)
         {
-            string convertedNumber = null;
+            string[] hundredsLetters = { "", "C" };
+            string[] tensLetters = { "", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC" };
+            string[] onesLetters = { "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" };
 
+            string romanValue = "";
+            int number;
+            //numbers with 3 decimals
+            number = numberToConvert / 100;
+            romanValue += hundredsLetters[number];
+            numberToConvert %= 100;
 
-            return convertedNumber;
+            //numbers with 2 decimals
+            number = numberToConvert / 10;
+            romanValue += tensLetters[number];
+            numberToConvert %= 10;
+
+            //numbers with one decimal
+            romanValue += onesLetters[numberToConvert];
+
+            return romanValue;
         }
     }
 }
