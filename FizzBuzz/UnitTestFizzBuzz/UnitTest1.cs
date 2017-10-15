8d6cdbf2e6f24db7b7bf1c7a47df22091917fe84
radunica94 <32551335+radunica94@users.noreply.github.com>
2017-10-15 14:42:14 +0300

    buzz condition

--- FizzBuzz/UnitTestFizzBuzz/UnitTest1.cs
+++ FizzBuzz/UnitTestFizzBuzz/UnitTest1.cs
@@ -27,6 +27,8 @@ namespace UnitTestFizzBuzz
         {
             if (value % 3 == 0)
                 return "Fizz";
+            if (value % 5 == 0)
+                return "Buzz";
             else
                 return "NoFizzBuzz";
         }
