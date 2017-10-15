af383c8f5cf8cd7cd451c2c0f62f52a8d960728d
radunica94 <32551335+radunica94@users.noreply.github.com>
2017-10-15 19:04:45 +0300

    field problem

---
+++ FarmersField/UnitTestFarmersField/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestFarmersField")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestFarmersField")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d4e63dc9-25be-44e4-8206-592ac7fc31de")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ FarmersField/UnitTestFarmersField/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestFarmersField
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(592900,CalculateFieldSize(230,770000));
+        }
+        double CalculateFieldSize(int addedSize , int totalArea)
+        {
+            return 0;
+        }
+    }
+}
