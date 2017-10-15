e35f941da68cae4b35de77feeebf62f977bd5668
radunica94 <32551335+radunica94@users.noreply.github.com>
2017-10-15 16:55:14 +0300

    get debt function

---
+++ Rent/UnitTestRent/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestRent")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestRent")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("15e070ac-d862-40e5-943c-ed6bbce2f821")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Rent/UnitTestRent/UnitTest1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestRent
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(108, CalculateDebts(100, 4));
+        }
+
+        double CalculateDebts(int rent , int delayedDays )
+        {
+            return GetDebt(rent, delayedDays) + rent;
+        }
+
+        double GetDebt(int rent , int delayedDays)
+        {
+            if (delayedDays >= 1 && delayedDays <= 10)
+                return rent * 0.02 * delayedDays;
+            if (delayedDays >= 11 && delayedDays <= 30)
+                return rent * 0.05 * delayedDays;
+            if (delayedDays >= 31 && delayedDays <= 40)
+                return rent * 0.1 * delayedDays;
+            else
+                return rent;
+        }
+
+    }
+}
