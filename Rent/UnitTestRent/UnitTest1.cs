be92f4e25095959fc3fa762e7dcc58e4db36fd5a
radunica94 <32551335+radunica94@users.noreply.github.com>
2017-10-15 16:58:35 +0300

    test for each case

--- Rent/UnitTestRent/UnitTest1.cs
+++ Rent/UnitTestRent/UnitTest1.cs
@@ -11,12 +11,27 @@ namespace UnitTestRent
         {
             Assert.AreEqual(108, CalculateDebts(100, 4));
         }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.AreEqual(175, CalculateDebts(100, 15));
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Assert.AreEqual(460, CalculateDebts(100, 36));
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Assert.AreEqual(100, CalculateDebts(100, 0));
+        }
 
         double CalculateDebts(int rent , int delayedDays )
         {
             return GetDebt(rent, delayedDays) + rent;
         }
-
+        
         double GetDebt(int rent , int delayedDays)
         {
             if (delayedDays >= 1 && delayedDays <= 10)
@@ -26,7 +41,7 @@ namespace UnitTestRent
             if (delayedDays >= 31 && delayedDays <= 40)
                 return rent * 0.1 * delayedDays;
             else
-                return rent;
+                return 0;
         }
 
     }
