edb0287855283202766a3d252a9a2698cacaa772
radunica94 <32551335+radunica94@users.noreply.github.com>
2017-10-15 18:14:09 +0300

    return "NU" if melonWeight = 2

--- Watermelon/UnitTestWatermelon/UnitTest1.cs
+++ Watermelon/UnitTestWatermelon/UnitTest1.cs
@@ -18,8 +18,16 @@ namespace UnitTestWatermelon
             Assert.AreEqual("NU", CalculateMelonWeight(11));
         }
 
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Assert.AreEqual("NU", CalculateMelonWeight(2));
+        }
+
         string CalculateMelonWeight(int melonWeight)
         {
+            if (melonWeight == 2)
+                return "NU";
             if(melonWeight %2 == 0) 
                 return "DA";
             return "NU";
