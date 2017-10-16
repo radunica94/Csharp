ff4797a4274e7511d2b5ff9afb002947b1a86eea
radunica94 <32551335+radunica94@users.noreply.github.com>
2017-10-16 18:21:36 +0300

    Creating the first test

---
+++ Prefix/UnitTestPrefix/Prefix.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestPrefix
+{
+    [TestClass]
+    public class Prefix
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual("aaa",FindThePrefix("aaab", "aaaabbaa"));
+        }
+
+        string FindThePrefix(string firstValue, string secondValue)
+        {
+            return "";
+        }
+    }
+}
