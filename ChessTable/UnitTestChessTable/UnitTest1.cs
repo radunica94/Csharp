650e43c8da7fd71e9fb15c1218abd689361a27ca
radunica94 <32551335+radunica94@users.noreply.github.com>
2017-10-16 17:54:22 +0300

    chess table first test passed

--- ChessTable/UnitTestChessTable/UnitTest1.cs
+++ ChessTable/UnitTestChessTable/UnitTest1.cs
@@ -12,9 +12,9 @@ namespace UnitTestChessTable
             Assert.AreEqual(204,CalculateTheNumberOfSquares(8));
         }
 
-        int CalculateTheNumberOfSquares(int value)
+        double CalculateTheNumberOfSquares(int value)
         {
-            return 0;
+            return Math.Pow(value-7, 2) + Math.Pow(value-6, 2) + Math.Pow(value-5, 2) + Math.Pow(value-4, 2) + Math.Pow(value-3, 2) + Math.Pow(value-2, 2) + Math.Pow(value-1, 2) + Math.Pow(value, 2);
         }
     }
 }
