9e24c6d4fafa978759f9ef900b7b003241cdd839
radunica94 <32551335+radunica94@users.noreply.github.com>
2017-10-18 15:05:36 +0300

    panagram problem.

---
+++ Pangram/UnitTestPangram/Pangram.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestPangram
+{
+    [TestClass]
+    public class Pangram
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(true,CheckForPangram("The quick brown fox jumps over the lazy dog"));
+        }
+
+        bool CheckForPangram(string sentence)
+        {
+            bool panagram = true;
+            return panagram;
+        }
+    }
+}
---
+++ Pangram/UnitTestPangram/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestPangram")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestPangram")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a6106a23-12eb-44ad-a580-3e7190561614")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
