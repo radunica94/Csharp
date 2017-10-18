ffaf376976c1b62716b59ee1a08350568d0627c4
radunica94 <32551335+radunica94@users.noreply.github.com>
2017-10-18 16:38:05 +0300

    first green test

--- Excel/UnitTestExcel/Excel.cs
+++ Excel/UnitTestExcel/Excel.cs
@@ -9,12 +9,24 @@ namespace UnitTestExcel
         [TestMethod]
         public void TestMethod1()
         {
-            Assert.AreEqual(27,ExelColumns("AA"));
+            Assert.AreEqual("AA",ExelColumns(27));
         }
 
-        int ExelColumns(string columnName)
+        string ExelColumns(int columnID)
         {
-            return 0;
+            int id = columnID;
+            string columnLetter = string.Empty;
+            int mod = 0;
+
+            while (id > 0)
+            {
+                mod = (id - 1) % 26;
+                columnLetter = (char)(65 + mod) + columnLetter;
+                id = (int)((id - mod) / 26);
+            }
+
+
+            return columnLetter;
         }
     }
 }
