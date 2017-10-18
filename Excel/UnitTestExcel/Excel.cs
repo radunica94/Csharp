58cda93bb672f472dc983cfa7885a2001dfdc02e
radunica94 <32551335+radunica94@users.noreply.github.com>
2017-10-18 16:28:36 +0300

    First test.

--- Excel/UnitTestExcel/Excel.cs
+++ Excel/UnitTestExcel/Excel.cs
@@ -4,11 +4,17 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace UnitTestExcel
 {
     [TestClass]
-    public class UnitTest1
+    public class Excel
     {
         [TestMethod]
         public void TestMethod1()
         {
+            Assert.AreEqual(27,ExelColumns("AA"));
+        }
+
+        int ExelColumns(string columnName)
+        {
+            return 0;
         }
     }
 }
