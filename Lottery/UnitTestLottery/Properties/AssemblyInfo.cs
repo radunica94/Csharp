9a1de30adca8611cdd748513b61d6d1615ba34b2
radunica94 <32551335+radunica94@users.noreply.github.com>
2017-10-20 17:14:06 +0300

    Empty Lottery Project

---
+++ Lottery/UnitTestLottery/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestLottery")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestLottery")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2a191a93-9633-44aa-9cf0-c7527609cfb6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Lottery/UnitTestLottery/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestLottery
+{
+    [TestClass]
+    public class Lottery
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(0.00000007, CalculateTheWinningChance(49, 6));
+        }
+
+        double CalculateTheWinningChance(int totalNumber, int winningNumbers)
+        {
+            return 0;
+        }
+
+    }
+}
