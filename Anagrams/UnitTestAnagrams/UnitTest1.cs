31425af7dbbb00f1eb2b3aa8d491f744f06592b6
radunica94 <32551335+radunica94@users.noreply.github.com>
2017-10-20 19:11:22 +0300

    first and second green test.

--- Anagrams/UnitTestAnagrams/UnitTest1.cs
+++ Anagrams/UnitTestAnagrams/UnitTest1.cs
@@ -12,21 +12,34 @@ namespace UnitTestAnagrams
             Assert.AreEqual(1,CalculateAnagrams("a"));
         }
 
-        double CalculateAnagrams(string word)
+        [TestMethod]
+        public void TestForFourLetters()
         {
-            return Factorial(word.Length);
+            Assert.AreEqual(24, CalculateAnagrams("abcd"));
         }
 
-        double Factorial(int i)
+        int CalculateAnagrams(string word)
+        {
+            int fact = 1;
+            for (int i = 'a'; i <= 'z'; i++)
+                fact *= Factorial(FindRepeatingLetter(i, word));
+            return Factorial(word.Length)/fact;
+        }
+
+        int Factorial(int i)
         {
             if (i <= 1)
                 return 1;
             return i * Factorial(i - 1);
         }
 
-        int RepeatingLetter()
+        int FindRepeatingLetter(int letterToCompare, string word)
         {
-            return 0;
+            int index = 0;
+            for (int i = 0; i < word.Length; i++)
+                if (letterToCompare == word[i])
+                    index++;
+            return index;
         }
     }
 }
