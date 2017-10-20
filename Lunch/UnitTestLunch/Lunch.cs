f104d82e27b27e9e06ef599cc9f7b1abecec4932
radunica94 <32551335+radunica94@users.noreply.github.com>
2017-10-20 19:34:21 +0300

    final function

--- Lunch/UnitTestLunch/Lunch.cs
+++ Lunch/UnitTestLunch/Lunch.cs
@@ -4,11 +4,38 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace UnitTestLunch
 {
     [TestClass]
-    public class UnitTest1
+    public class Lunch
     {
         [TestMethod]
         public void TestMethod1()
         {
+            Assert.AreEqual(12,CalculateLunchSchedule(6,4));
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.AreEqual(4, CalculateLunchSchedule(4, 4));
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Assert.AreEqual(4, CalculateLunchSchedule(4, 2));
+        }
+        int CalculateLunchSchedule(int mySchedule, int friendsSchedule)
+        {
+            int sum = mySchedule * friendsSchedule;
+            if (mySchedule == friendsSchedule)
+                return mySchedule;
+            if (sum % CalculateDifference(mySchedule, friendsSchedule) == 0)
+                return sum / CalculateDifference(mySchedule, friendsSchedule);
+            return sum;
+        }
+
+        private int CalculateDifference(int mySchedule, int friendsSchedule)
+        {
+            if (mySchedule > friendsSchedule)
+                return mySchedule - friendsSchedule;
+            return friendsSchedule - mySchedule;
         }
     }
 }
