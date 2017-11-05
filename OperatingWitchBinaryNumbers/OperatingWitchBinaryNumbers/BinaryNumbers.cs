eb17468914889ce67083b76e11dfb0cf0fff3302
radunica94 <32551335+radunica94@users.noreply.github.com>
2017-11-05 16:38:53 +0200

    AND OR and XOR green test

--- OperatingWitchBinaryNumbers/OperatingWitchBinaryNumbers/BinaryNumbers.cs
+++ OperatingWitchBinaryNumbers/OperatingWitchBinaryNumbers/BinaryNumbers.cs
@@ -33,5 +33,53 @@ namespace OperatingWitchBinaryNumbers
             }
             return binary;
         }
+        public byte[] LogicOperations(byte[] firstValue, byte[] secondValue, string operation)
+        {
+            byte[] result = new byte[Math.Max(firstValue.Length, secondValue.Length)];
+            for (int i = 0; i < result.Length; i++)
+            {
+                byte firstValueByte = AddZero(firstValue, i);
+                byte secondValueByte = AddZero(secondValue, i);
+                result[i] = LogicOperations(firstValueByte, secondValueByte, operation);
+            }
+            return ReverseBinary(result);
+        }
+        public byte ANDOperator(byte firstValue, byte secondValue)
+        {
+            if (firstValue == secondValue && firstValue == 1)
+                return 1;
+            return 0;
+        }
+        public byte OROperator(byte firstValue, byte secondValue)
+        {
+            if (firstValue == 1 || secondValue == 1)
+                return 1;
+            return 0;
+        }
+        public byte XOROperator(byte firstValue, byte secondValue)
+        {
+            if (firstValue != secondValue)
+                return 1;
+            return 0;
+        }
+        public byte LogicOperations(byte firstValue, byte secondValue, string operation)
+        {
+            switch (operation)
+            {
+                case "AND":
+                    return ANDOperator(firstValue, secondValue);
+                case "OR":
+                    return OROperator(firstValue, secondValue);
+                case "XOR":
+                    return XOROperator(firstValue, secondValue);
+            }
+            return 0;
+        }
+        public byte AddZero(byte[] number, int position)
+        {
+            if (position >= (number.Length))
+                return (byte)0;
+            return number[number.Length - position - 1];
+        }
     }
 }
--- OperatingWitchBinaryNumbers/OperatingWitchBinaryNumbers/BinaryNumbersTest.cs
+++ OperatingWitchBinaryNumbers/OperatingWitchBinaryNumbers/BinaryNumbersTest.cs
@@ -18,5 +18,29 @@ namespace OperatingWitchBinaryNumbers
             var firstValue = new BinaryNumbers();
             Assert.Equal(new byte[] { 1, 1, 1, 1 }, firstValue.ConvertToBinary(15));
         }
+        [Fact]
+        public void ANDOperator()
+        {
+            var firstValue = new BinaryNumbers();
+            var secondValue = new BinaryNumbers();
+            var result = new BinaryNumbers();
+            Assert.Equal(new byte[] { 0, 0, 1 }, result.LogicOperations(firstValue.ConvertToBinary(1), secondValue.ConvertToBinary(7), "AND"));
+        }
+        [Fact]
+        public void OROperator()
+        {
+            var firstValue = new BinaryNumbers();
+            var secondValue = new BinaryNumbers();
+            var result = new BinaryNumbers();
+            Assert.Equal(new byte[] { 1, 1, 1 }, result.LogicOperations(firstValue.ConvertToBinary(1), secondValue.ConvertToBinary(7), "OR"));
+        }
+        [Fact]
+        public void XOROperator()
+        {
+            var firstValue = new BinaryNumbers();
+            var secondValue = new BinaryNumbers();
+            var result = new BinaryNumbers();
+            Assert.Equal(new byte[] { 1, 1, 0 }, result.LogicOperations(firstValue.ConvertToBinary(1), secondValue.ConvertToBinary(7), "XOR"));
+        }
     }
 }
