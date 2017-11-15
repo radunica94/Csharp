836343210dd55b622412ef852f5f23f212288699
radunica94 <32551335+radunica94@users.noreply.github.com>
2017-11-15 18:52:25 +0200

    implementation for small letters

--- Password/Password/Password.cs
+++ Password/Password/Password.cs
@@ -6,40 +6,40 @@ namespace Password
 {
     class Password
     {
-    }
-    public struct PasswordSettings
-    {
-        public int smallLetters, capitalLetters, digits, symbols;
-        public bool noSimilarCharacters, noAmbiguousChars;
-        public PasswordSettings(int a, int b, int c, int d, bool similar, bool ambiguous)
+        public struct PasswordSettings
         {
-            smallLetters = a;
-            capitalLetters = b;
-            digits = c;
-            symbols = d;
-            noSimilarCharacters = similar;
-            noAmbiguousChars = ambiguous;
+            public int smallLetters, capitalLetters, digits, symbols;
+            public bool noSimilarCharacters, noAmbiguousChars;
+            public PasswordSettings(int a, int b, int c, int d, bool similar, bool ambiguous)
+            {
+                smallLetters = a;
+                capitalLetters = b;
+                digits = c;
+                symbols = d;
+                noSimilarCharacters = similar;
+                noAmbiguousChars = ambiguous;
+            }
         }
-    }
 
-    public char[] GeneratePassword(PasswordSettings settings, int passwordLength)
-    {
-        Random random = new Random();
-        char[] finalPassword = new char[passwordLength];
-        var chars = "abcdefghijklmnopqrstuvwxyz";
-        for (int i = 0; i < finalPassword.Length; i++)
+        public char[] GeneratePassword(PasswordSettings settings, int passwordLength)
         {
-            finalPassword[i] = chars[random.Next(chars.Length)];
+            Random random = new Random();
+            char[] finalPassword = new char[passwordLength];
+            var chars = "abcdefghijklmnopqrstuvwxyz";
+            for (int i = 0; i < finalPassword.Length; i++)
+            {
+                finalPassword[i] = chars[random.Next(chars.Length)];
+            }
+            return finalPassword;
         }
-        return finalPassword;
-    }
 
-    public bool CheckPassword(char[] password)
-    {
-        for (int i = 0; i < password.Length; i++)
-            if (password[i] >= 'a' && password[i] <= 'z')
-                return true;
-        return false;
+        public bool CheckPassword(char[] password)
+        {
+            for (int i = 0; i < password.Length; i++)
+                if (password[i] >= 'a' && password[i] <= 'z')
+                    return true;
+            return false;
 
+        }
     }
 }
