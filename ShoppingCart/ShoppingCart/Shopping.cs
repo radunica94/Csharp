97d6fa11ce2cbf942e9c165e8ae45892bda8919f
radunica94 <32551335+radunica94@users.noreply.github.com>
2017-12-05 17:08:34 +0200

    Shopping cart Project plus totalPrice and Cheapest Product method

---
+++ ShoppingCart/ShoppingCart/Shopping.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shopping
+{
+    class Shopping
+    {
+        private Product[] productList;
+        public struct Product
+        {
+            public int price;
+            public int numberOfProducts;
+            public Product(int price, int numberOfProducts)
+            {
+                this.price = price;
+                this.numberOfProducts = numberOfProducts;
+            }
+        }
+        public Shopping(Product[] productList)
+        {
+            this.productList = productList;
+        }
+
+        public int TotalPrice()
+        {
+            int totalPrice = 0;
+            for (int i = 0; i < productList.Length; i++)
+            {
+                totalPrice += productList[i].numberOfProducts * productList[i].price;
+            }
+            return totalPrice;
+        }
+
+        public Product CheapestProduct()
+        {
+            int cheapestProduct = productList[0].price;
+            int counter = 0;
+            for (int i = 0; i < productList.Length; i++)
+            {
+                if (productList[i].price < cheapestProduct)
+                {
+                    cheapestProduct = productList[i].price;
+                    counter = i;
+                }
+            }
+            return productList[counter];
+        }
+    }
+}
+
+
---
+++ ShoppingCart/ShoppingCart/ShoppingCartTest.cs
@@ -0,0 +1,32 @@
+using System;
+using Xunit;
+
+namespace Shopping
+{
+    public class ShoppingCartTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            Shopping.Product result = new Shopping.Product(10, 2);
+            var price = result.price;
+            Assert.Equal(10, price);
+        }
+        [Fact]
+        public void TotalPrice()
+        {
+            Shopping.Product[] result = new Shopping.Product[] { new Shopping.Product(5, 1), new Shopping.Product(15, 2), new Shopping.Product(2, 1) };
+            Shopping productList = new Shopping(result);
+            var method = productList.TotalPrice();
+            Assert.Equal(37, method);
+        }
+        [Fact]
+        public void CheapestProduct()
+        {
+            Shopping.Product[] result = new Shopping.Product[] { new Shopping.Product(5, 1), new Shopping.Product(15, 1), new Shopping.Product(2, 1) };
+            Shopping productList = new Shopping(result);
+            var method = productList.CheapestProduct();
+            Assert.Equal(result[2], method);
+        }
+    }
+}
