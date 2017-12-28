54f5de8c5cf89c1281b771ea7caf6beacf1f4bfc
radunica94 <32551335+radunica94@users.noreply.github.com>
2017-12-28 12:28:18 +0200

    search for a product

--- OnlineShop/OnlineShop/OnlineShopClass.cs
+++ OnlineShop/OnlineShop/OnlineShopClass.cs
@@ -6,5 +6,45 @@ namespace OnlineShop
 {
     class OnlineShopClass
     {
+        private Product[] productList;
+        public struct Product
+        {
+            public string productName;
+            public decimal productPrice;
+            public int numberOfProducts;
+            public string description;
+            public Product(string productName, decimal productPrice, int numberOfProducts, string description)
+            {
+                this.productName = productName;
+                this.productPrice = productPrice;
+                this.numberOfProducts = numberOfProducts;
+                this.description = description;
+            }
+        }
+        public OnlineShopClass(Product[] productList)
+        {
+            this.productList = productList;
+        }
+        public bool Contains(Product product)
+        {
+            for (int i = 0; i < productList.Length; i++)
+                if (productList[i].Equals(product))
+                    return true;
+            return false;
+        }
+
+        public void SearchForProduct()
+        {
+            string searchedProduct = productList[0].productName;
+            int counter = 0;
+            for(int i = 0; i < productList.Length; i++)
+            {
+                if (productList[i].productName == searchedProduct)
+                {
+                    searchedProduct = productList[i].productName;
+                    counter = i;
+                }
+            }
+        }
     }
 }
--- OnlineShop/OnlineShop/OnlineShopTest.cs
+++ OnlineShop/OnlineShop/OnlineShopTest.cs
@@ -3,12 +3,26 @@ using Xunit;
 
 namespace OnlineShop
 {
-    public class UnitTest1
+    public class OnlineShopTest
     {
         [Fact]
-        public void Test1()
+        public void TestTheStruct()
         {
-
+            OnlineShopClass.Product result = new OnlineShopClass.Product("Samsung A5", 100, 3, "Model 2017");
+            var productPrice = result.productPrice;
+            Assert.Equal(100, productPrice);
+        }
+        [Fact]
+        public void SearchForProduct()
+        {
+            var searchedProduct = new OnlineShopClass.Product("Samsung Galaxy A5", 100, 3, "Model 2017");
+            var result = new OnlineShopClass.Product[] {
+                new OnlineShopClass.Product("Iphone X", 100, 3, "Model 2017"),
+                searchedProduct,
+                new OnlineShopClass.Product("Samsung Galaxy S8", 100, 3, "Model 2017") };
+            var listOfProducts = new OnlineShopClass(result);
+            listOfProducts.SearchForProduct();
+            Assert.True(listOfProducts.Contains(searchedProduct));
         }
     }
 }
